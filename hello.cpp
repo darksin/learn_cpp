e95068dda8c19b69fb73fd69a49ca38b58f367c5
Sebastian <darksinkk@gmail.com>
2016-07-06 13:45:39 +0200

    Variables

--- hello.cpp
+++ hello.cpp
@@ -3,6 +3,16 @@ using namespace std;
 
 int main()
 {
-    std::cout << "Hello world!" << std::endl;
+    int aktualnyRok = 2016;
+    int a = 10;
+    int b = 12;
+    int suma = a + b;
+    std::cout << "Hello world! in ";
+    std::cout << aktualnyRok << std::endl;
+    std::cout << "Suma a i b wynosi ";
+    std::cout << suma << std::endl;
+    int c;
+    std::cout << "Podaj wartość c: ";
+    std::cin >> c;
     return 0;
 }
