e9dc190db929bf10bbcf15ef43322ef3588b11be
Sebastian <darksinkk@gmail.com>
2016-07-06 23:54:21 +0200

    Cleanup

---
+++ basic_concepts.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    /* Variables */
+    int aktualnyRok = 2016;
+    int a = 10;
+    int b = 12;
+    int suma = a + b;
+    std::cout << "Hello world! in " << aktualnyRok << std::endl;
+    std::cout << "Suma a i b wynosi " << suma << std::endl;
+    int c;
+    std::cout << "Podaj wartość c: ";
+    std::cin >> c;
+    std::cout << c << std::endl;
+
+    return 0;
+}
--- conditionals_and_loops.cpp
+++ conditionals_and_loops.cpp
@@ -4,15 +4,8 @@ using namespace std;
 int main()
 {
     /* Variables */
-    int aktualnyRok = 2016;
     int a = 10;
     int b = 12;
-    int suma = a + b;
-    std::cout << "Hello world! in " << aktualnyRok << std::endl;
-    std::cout << "Suma a i b wynosi " << suma << std::endl;
-    int c;
-    std::cout << "Podaj wartość c: ";
-    std::cin >> c;
 
     /* if else statement */
     if ( b > a ) {
