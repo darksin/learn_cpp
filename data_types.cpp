1f7cd186dd7f2e0a53122a176a3a1ef592226d46
Sebastian <darksinkk@gmail.com>
2016-07-07 07:03:12 +0200

    Arrays

--- data_types.cpp
+++ data_types.cpp
@@ -18,6 +18,34 @@ int main()
     std::cout << c << std::endl;
     char test = 'S';
     std::cout << test << std::endl;
+
+    /* Variable naming rules
+    Arrays */
+    int i[5];
+    int j[5] = {11, 45, 62, 70, 88};
+    j[1] = 42;
+    std::cout << "Array: " << j[1] << std::endl;
+
+    /* Arrays in Loops */
+    int myArr[5];
+
+    for(int x=0; x<5; x++) {
+        myArr[x] = 42;
+        cout << x << ": " << myArr[x] << endl;
+    }
+
+    /* Arrays in Calculations */
+    int arr[] = {11, 35, 62, 555, 989};
+    int sum = 0; 
+
+    for (int x = 0; x < 5; x++) {
+        sum += arr[x];
+    }
+    cout << sum << endl;
+
+    /* Multi-Dimensional Arrays */
+    int x[2][3] = {{2, 3, 4}, {8, 9, 10}};
+    cout << x[0][2] << endl;
     
     return 0;
 }
