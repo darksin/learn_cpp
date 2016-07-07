fa3a7b5f3cb6ed24a4405b41a96753bfd64df11f
Sebastian <darksinkk@gmail.com>
2016-07-07 06:59:40 +0200

    Data types

---
+++ data_types.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    /* Data types
+    int, float, double */
+    int a = 42;
+    std::cout << "Integer: " << a << std::endl;
+    unsigned long int b;
+    std::cout << "Unsigned long integer: " << b << std::endl;
+    double temp = 4.21;
+    std::cout << "Float: " << temp << std::endl;
+
+    return 0;
+}
