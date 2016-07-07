d43fc6d0649bac16b55436ca187aec268a06b7b4
Sebastian <darksinkk@gmail.com>
2016-07-07 13:08:34 +0200

    Functions PArameters

--- functions.cpp
+++ functions.cpp
@@ -8,14 +8,22 @@ void printSomething() // 1st option
 }
 
 void printSomethingAgain(); // 2nd option
+int timesTwo(int x);
 
 int main()
 {
     printSomething();
     printSomethingAgain();
+
+    cout << timesTwo(4) << endl;
+    cout << timesTwo(24) << endl;
     return 0;
 }
 
 void printSomethingAgain() { // 2nd option
     cout << "2nd option" << endl;
+}
+
+int timesTwo(int x) {
+    return x+2;
 }
