a80acef1c4a2c35b8eb77949f01d851f35a07ecd
darksin <darksinkk@gmail.com>
2016-09-30 19:23:08 +0200

    Class Templates

--- Templates_Exceptions_and_Files.cpp
+++ Templates_Exceptions_and_Files.cpp
@@ -5,8 +5,27 @@ T smaller(T a, U b) {
     return (a<b ? a : b);
 }
 
+template <class T>
+class Pair {
+    private:
+        T first,second;
+    public:
+        Pair (T c, T d):
+            first(c), second(d) {
+
+            }
+        T bigger();
+};
+
+template <class T>
+T Pair<T>::bigger(){
+    return (first>second ? first : second);
+}
+
 int main() {
     int x=7.15;
     double y=15.54;
     std::cout << smaller(x,y) << std::endl;
+    Pair <int> obj(11,22);
+    std::cout << obj.bigger();
 }
