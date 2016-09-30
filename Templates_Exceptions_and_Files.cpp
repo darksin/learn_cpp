8da17f067e3c74977ed354430de1acad2148b70f
darksin <darksinkk@gmail.com>
2016-09-30 19:31:03 +0200

    Template Specialization

--- Templates_Exceptions_and_Files.cpp
+++ Templates_Exceptions_and_Files.cpp
@@ -22,10 +22,29 @@ T Pair<T>::bigger(){
     return (first>second ? first : second);
 }
 
+template <class T>
+class MyClass {
+    public:
+        MyClass(T x) {
+            std::cout << x << " - not a char" << std::endl;
+        }
+};
+
+template < >
+class MyClass<char> {
+    public:
+        MyClass (char x) {
+            std::cout << x << " - is a char!" << std::endl;
+        }
+};
+
 int main() {
     int x=7.15;
     double y=15.54;
     std::cout << smaller(x,y) << std::endl;
     Pair <int> obj(11,22);
     std::cout << obj.bigger();
+    MyClass<int>ob1(42);
+    MyClass<double>ob2(5.47);
+    MyClass<char>ob3('s');
 }
