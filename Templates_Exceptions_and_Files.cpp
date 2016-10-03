df06627b882d49193c45b5732d79111eadd985de
darksin <darksinkk@gmail.com>
2016-10-03 17:37:39 +0200

    More on Exceptions

--- Templates_Exceptions_and_Files.cpp
+++ Templates_Exceptions_and_Files.cpp
@@ -58,4 +58,19 @@ int main() {
     catch (int x) {
         std::cout << "Wrong age values - ERROR " << x << std::endl;
     }
+    try {
+        int num1;
+        std::cout << "Enter the first number" << std::endl;
+        std::cin >> num1;
+        int num2;
+        std::cout << "Enter the second number" << std::endl;
+        std::cin >> num2;
+        if(num2 == 0) {
+            throw 0;
+        }
+        std::cout << "Result: " << num1 / num2 << std::endl;
+    }
+    catch(int x) {
+        std::cout << "Division by zero! " << std::endl;
+    }
 }
